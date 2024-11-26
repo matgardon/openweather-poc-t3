e38bd90e05912bd059139359ceaadc6c01a033b2
Mathis Gardon <mathis.gardon@gmail.com>
2024-11-26 02:16:19 +0100

    feat (favoriteLocations) add router and display favorite locations on homepage when user is authenticated

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { geocodingRouter } from "./routers/geocoding";
 import { weatherRouter } from "./routers/weather";
+import { favoriteLocationRouter } from "./routers/favoriteLocation";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { weatherRouter } from "./routers/weather";
 export const appRouter = createTRPCRouter({
   geocoding: geocodingRouter,
   weather: weatherRouter,
+  favoriteLocation: favoriteLocationRouter,
 });
 
 // export type definition of API
